d4abcef8d5c9a617d00d43efc4b92fa5183d4041
GeordiF <geordi.feijens@student.kdg.be>
2017-10-05 11:35:32 +0200

    opdracht-concatenate oplossing

---
+++ Oefeningen/opdracht-concatenate-solved.php
@@ -0,0 +1,22 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+   
+   <?php 
+    
+    $Familienaam = 'Feijens'; 
+    $Voornaam = 'Geordi'; 
+    $Naam = $Voornaam . ' ' . $Familienaam; 
+    
+    echo $Naam . '<br>' . strlen($Naam); 
+    ?>
+    
+</body>
+</html>
