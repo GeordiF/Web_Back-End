de9bb40ec7d6e4f2743af17ab8ce802a0533103e
GeordiF <geordi.feijens@student.kdg.be>
2018-01-19 16:27:25 +0100

    laracasts aan het volgen

---
+++ Hackernews_opdracht/resources/views/pages/about.blade.php
@@ -0,0 +1,11 @@
+@extends('pages.layout')
+
+@section('header')
+<title>About</title>
+@stop
+
+@section('content')
+
+<h1>About Page</h1>
+
+@stop
---
+++ Hackernews_opdracht/resources/views/pages/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('pages.layout')
+
+@section('header')
+<title>Home</title>
+@stop
+
+@section('content')
+
+<h1>Welcome Page</h1>
+
+@stop
